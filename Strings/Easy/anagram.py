1a9577246b17359d6f1058ee36e6aa0b50ec9f43
pooja-c03 <pooja0909.c@gmail.com>
2025-02-28 10:22:58 +0530

    anagram using package

---
+++ Strings/Easy/anagram.py
@@ -0,0 +1,19 @@
+# Check if 2 strings are anagram or no
+
+#from collection package
+
+from collections import Counter
+
+def anagram(s1,s2):
+    if len(s1) != len(s2):
+        print("Not Anagram.")
+    else:
+        if Counter(s1) == Counter(s2):
+            print("Anagram.")
+        else:
+            print("Not Anagram.")
+
+if __name__=="__main__":
+    s1 = input("Enter 1st string: ")
+    s2 = input("Enter 2nd string: ")
+    anagram(s1,s2)
